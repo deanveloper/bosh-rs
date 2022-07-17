71a5c47999afd0ebff752431f19e94c5f9547b84
dean <dean@dbassett.dev>
2022-07-16 23:09:38 -0700

    separate Bosh into Bosh and Sled

--- src/bosh.rs
+++ src/bosh.rs
@@ -5,12 +5,20 @@ pub struct Link(pub Point, pub Point);
 pub struct Bosh {
     pub right_foot: Point,
     pub left_foot: Point,
-    pub peg: Point,
-    pub tail: Point,
-    pub nose: Point,
-    pub rope: Point,
     pub left_arm: Point,
     pub right_arm: Point,
     pub shoulder: Point,
     pub butt: Point,
+}
+
+pub struct Sled {
+    pub peg: Point,
+    pub nose: Point,
+    pub tail: Point,
+    pub rope: Point,
+}
+
+pub struct BoshSled {
+    pub bosh: Bosh,
+    pub sled: Sled,
 }
