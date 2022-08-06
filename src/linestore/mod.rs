3d2ed4a38d9c3f779518b3a8387367f5a752c1aa
dean <dean@dbassett.dev>
2022-08-06 12:01:26 -0700

    add all_lines test

--- src/linestore/mod.rs
+++ src/linestore/mod.rs
@@ -203,7 +203,7 @@ mod tests {
         let line4 = make_line(Vector2D(2.0, 0.0), Vector2D(100.0, 0.0));
         let far_line = make_line(Vector2D(0.0, 1000.0), Vector2D(100.0, 1000.0));
 
-        let grid = Grid::new(&vec![line1, line2, line3, far_line]);
+        let grid = Grid::new(&vec![line1, line2, line3, line4, far_line]);
 
         let lines = grid.all_lines();
         assert_eq!(lines, &vec![line1, line2, line3, line4, far_line]);
